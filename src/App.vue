85f032832ba1666ceca3d827b3c909c8a0c6758b
NAre5 <sherlord5@gmail.com>
2020-06-02 01:50:08 +0300

    test commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <u><h1>My Beautiful site</h1></u>
+    <mycomponent></mycomponent>
+  </div>
+</template>
+
+<script>
+import mycomponent from './components/my-component';
+export default {
+  name: "App",
+  components: {
+    mycomponent
+  }
+};
+</script>
---
+++ src/components/my-component.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">Hello {{who}}</div>
+</template>
+<script>
+export default {
+  data: function() {
+    return {
+      who: 'world'
+    }
+  },
+  created() {
+    // console.log(1234);
+  },
+}
+</script>
