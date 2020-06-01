ab73550d28026d324229c067eab0ae2cd3fa6377
NAre5 <sherlord5@gmail.com>
2020-06-02 02:08:32 +0300

    init2

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
-    <u><h1>My Beautiful site</h1></u>
+    <u><h1>My Beautiful site!!!</h1></u>
     <mycomponent></mycomponent>
   </div>
 </template>
 
 <script>
-import mycomponent from './components/my-component';
+import mycomponent from "./components/my-component";
 export default {
   name: "App",
   components: {
